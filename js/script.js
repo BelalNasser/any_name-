6660f38d2b9117e9543a311b96dca104281df5f3
BelalNasser <belal.nasser707@gmail.com>
2022-10-16 18:31:19 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// print text
+//document.write("Belal"); 
+
+
+// another way 
+
+//document.getElementById("result").innerHTML = "Belal Nasser" 
+
+// document.getElementById("btn").onclick = function(){
+
+//     document.getElementById("result").innerHTML = "Web Develop" 
+
+// }
+
+
+console.log("Ahmed")
